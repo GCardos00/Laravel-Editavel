f14045967fd86df6e19219fe5a2667a97be95222
Gustavoeteczl <gustavo.barros54@etec.sp.gov.br>
2022-09-30 21:02:53 -0300

    Projeto avaliativo referente a matéria de PW II

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prova de PW</title>
+</head>
+
+<body>
+    <center>
+         <h1>Escolha uma das Opções</h1>
+
+        <a href="/cadastro">Cadastrar Usuário</a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="/listar">Listar Cadastros</a>
+    </center>
+</body>
+
+</html>
